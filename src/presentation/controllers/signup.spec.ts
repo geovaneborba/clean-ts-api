ac651ef0544fba3011f34464a1d39da884ced5fa
Geovane Borba <geovanne@live.com>
2022-05-10 00:24:19 -0300

    test: ensure SignUpController return 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -18,4 +18,22 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  it('should return 400 if no email is provided', () => {
+    // sistem under test (sut)
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: "john doe",
+        password: '123456',
+        passwordConfirmation: '123456'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
